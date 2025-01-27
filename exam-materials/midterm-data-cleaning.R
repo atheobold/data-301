3fd3fa63d9feffbfd18dfcf92af32e9d960025e6
Allison Theobold <atheobol@calpoly.edu>
2025-01-26 18:55:55 -0800

    code for cleaning midterm dataset, final dataset

--- exam-materials/midterm-data-cleaning.R
+++ exam-materials/midterm-data-cleaning.R
@@ -43,4 +43,49 @@ coffee_clean <- coffee_raw |>
     color
     )
 
-write_csv(coffee_clean, file = "coffee_clean.csv")  
+## Need to Split Elevation Variable and Remove Letters from First Entry
+
+coffee_clean <- coffee_clean |> 
+  mutate(max_altitude = 
+           if_else(
+             str_length(altitude) > 4, 
+             str_extract(altitude, pattern = "[~-]\\s*\\d+"),
+             altitude
+           ), 
+         max_altitude = str_replace(max_altitude, 
+                                    pattern = "[~-]", 
+                                    replacement = ""), 
+         max_altitude = str_trim(max_altitude, side = "both"), 
+         min_altitude = if_else(
+           str_length(altitude) > 4, 
+           str_extract(altitude, pattern = "\\d+\\s*[~-]"),
+           altitude
+         ), 
+         min_altitude = str_replace(min_altitude, 
+                                    pattern = "[~-]", 
+                                    replacement = ""),
+         min_altitude = str_trim(min_altitude, side = "both"),
+         altitude_new = if_else(
+           min_altitude != max_altitude, 
+           str_c(min_altitude, "-", max_altitude, sep = ""), 
+           min_altitude
+         ), 
+         altitude_new = str_squish(altitude_new)
+    ) |> 
+  drop_na(max_altitude)
+
+## Need to Filter Processing Methods to Only be the top 3-4
+
+methods_to_keep <- tibble(processing_method = 
+                            c("Natural / Dry", 
+                              "Pulped natural / honey", 
+                              "Washed / Wet")
+                          )
+
+coffee_clean <- coffee_clean |> 
+  semi_join(methods_to_keep, by = "processing_method")
+
+coffee_clean |> 
+  select(-min_altitude, 
+         -max_altitude) |> 
+  write_csv(file = "coffee_clean.csv")  
